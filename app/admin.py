1eb0f4d8c01f564c0a187c67c6c2ee518ed73a2d
mlewis2257 <mlang.lewis@gmail.com>
2023-12-07 14:07:45 -0800

    setup

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Food
 
 # Register your models here.
+
+
+class Admin(admin.ModelAdmin):
+    list_display = ("title", "description", "completed")
+
+
+admin.site.register(Food, Admin)
